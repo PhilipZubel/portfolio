68e013812947edd55e1b11f2ca1674c139588a7a
PhilipZubel <philip.zubel@glasgow.ac.uk>
2022-05-23 15:57:44 +0200

    center cv button

--- src/assets/text.js
+++ src/assets/text.js
@@ -6,6 +6,7 @@
 const about = {
     description: "Hi, I'm Philip! 😎 I'm currently working as a software engineer at Glasgow University Software Services. I am also a final year Computing Science student at the University of Glasgow. 💻 I also love to code! ❤️ And I am keen on testing the limits of what can be achieved with a bit of imagination.",
     button : "View my Resume",
+    cvLink: "hello",
 }
 
 const experience = {};
