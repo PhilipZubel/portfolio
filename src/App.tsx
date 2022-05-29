a8ed2df9bd78b100eff22077ea5f1f6d896dcc32
PhilipZubel <philip.zubel@glasgow.ac.uk>
2022-05-29 21:14:31 +0200

    increase page size

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import Timeline from './components/timeline/Timeline';
 const App = () => {
   return (
     <>
-      <Parallax pages={6.3}>
+      <Parallax pages={6.35}>
         <ParallaxLayer
             speed={0.4}
             style ={{
